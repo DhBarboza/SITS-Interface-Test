69cb49288e9fc63cbd12c4e2035abbe276cf7e96
Douglas Henrique <barbozadh99@gmail.com>
2020-11-03 20:48:54 -0300

    Code Refatoring

--- app/server.R
+++ app/server.R
@@ -1,9 +1,70 @@
 # Import Packages:
 library(shiny)
 library(shinydashboard)
+library(ggsom)
+library(kohonen)
+library(tidyverse)
 
 server <- function(input, output, session) {
-  output$file <- renderTable({
-    input$chooseFile
-  })
-}
+  
+  output$view_file <- renderTable({
+    
+    req(input$chooseFile)
+    
+    tryCatch({
+      
+      df <- read.csv(
+        input$chooseFile$datapath,
+        header = input$header,
+        sep = input$separator,
+        quote = input$quote
+      )
+      
+      updateCheckboxGroupInput(session, "columns", "Select Columns", choices = names(df), selected = names(df))
+      
+      output$column_view <- renderTable({
+        
+        df <- subset(df, select = input$columns)
+        
+        v_som <- kohonen::som(scale(df), grid = somgrid(6,4, "rectangular"))
+        
+        v_plot <- eventReactive(input$showPlot, {
+          switch(
+            input$visualization,
+            "Line" = ggsom::ggsom_line(aes_som(model_som), input$change_color),
+            "Ribbon" = ggsom::ggsom_ribbon(aes_som(
+              model_som, cutree_value=input$slider_cluster_ribbon), TRUE),
+            "Rect" = ggsom::ggsom_rect(
+              aes_som(model_som, cutree_value=input$slider_cluster_rect),
+              input$neurons_grid
+            )
+          )# <- /switch
+          
+        })# <- /v_plot
+        
+        output$plot <- renderPlot({
+          v_plot()
+        })
+        
+        head(df, 10)
+        
+      })# <- /renderTable: column_view
+      
+    },
+    error = function(e) {
+      
+      stop(safeError(e))
+      
+    })#<- /Try Catch
+    
+    if (input$display == "head") {
+      return(head(df))
+    }
+    
+    else {
+      return(df)
+    }
+    
+  })# <- /renderTable: view_file
+  
+}# <- /function
--- app/ui.R
+++ app/ui.R
@@ -10,7 +10,23 @@ sidebar <- dashboardSidebar(width = 250,
                             
                             sidebarMenu(
                               
-                              menuItem("Import", tabName = "import", icon = icon("folder-open"))
+                              menuItem("Import", tabName = "import", icon = icon("folder-open")),
+                              
+                              menuItem("Quality Control Samples", icon = icon("chart-bar"),
+                                       
+                                       menuSubItem("Assess Quality", tabName = "quality", icon = icon("angle-right")),
+                                       
+                                       menuSubItem("Data Table", tabName = "dataTable", icon = icon("angle-right"))
+                                       
+                                    ),# <- /menuItem: Quality Control Samples
+                              
+                              menuItem("ST-Analysis", icon = icon("th"),
+                                       
+                                       menuSubItem("Analytic Simple", tabName = "analyticSimple", icon = icon("angle-right")),
+                                       
+                                       menuSubItem("Analytic Explore Subclasses", tabName = "exploreSubclasses", icon = icon("angle-right"))
+                                       
+                                    )# <- menuItem: ST-Analysis
                               
                             )# <- /Sidebar Menu
                             
@@ -18,54 +34,197 @@ sidebar <- dashboardSidebar(width = 250,
             # <--- /SIDEBAR --->
 
 body <- dashboardBody(
-          tabItems(
+  tabItems(
+  # <--------------------- <SIDEBAR TABS CONFIGURATION> -------------------------->  
+  # <--------------------- <TAB IMPORT> -------------------------->  
+    tabItem(tabName = "import",
+                tabBox(width = 13, 
+                       
+                       tabPanel("File Config",
+                                
+                                fluidRow(
+                                  
+                                  column(3, fileInput("chooseFile", "Choose File", multiple = FALSE),
+                                         
+                                                checkboxInput("header", "Header", TRUE)),
+                                         
+                                  column(1),
+                                         
+                                  column(3, radioButtons("separator", "Separator:", 
+                                                                choices = c(Comma = ",", Semicolon = ";", Tab = "\t"), 
+                                                                selected = ",")),
+                                         
+                                  column(3, radioButtons("quote", "Quote:", 
+                                                                choices = c(None = "", "Double Quote" = '"', "Single Quote" = "'"),
+                                                                selected = '"')),
+                                         
+                                  column(2, radioButtons("display", "Display:",
+                                                                choices = c(Head = "head", All = "all"),
+                                                                selected = "head"))
+                                  ),#<- /Fluid Row
+                                
+                                fluidRow( tableOutput("view_file"))
+                                
+                       ),#<- /Tab Panel: File Config
+                       
+                       
+                       
+                       tabPanel("Column View",
+                                fluidRow(
+                                  column(4, checkboxGroupInput("columns", "Select Columns:", choices = NULL))
+                                ),
+                                
+                                fluidRow(tableOutput("column_view"))
+                                
+                       ),#<- /Tab Panel: Column View
+                       
+                       tabPanel("Plot",
+                                fluidRow(
+                                  column(4,
+                                      
+                                      selectInput("visualization", "Choose a type of visualization",
+                                                  choices = c("Line", "Ribbon", "Rect")),
+                                      
+                                      conditionalPanel(condition = "input.visualization == 'Line'",
+                                                       checkboxInput("change_color", strong("Show color"), value = TRUE)),
+                                      
+                                      conditionalPanel(condition = "input.visualization == 'Ribbon'",
+                                                       sliderInput("slider_cluster_ribbon", "Choose the numbers of cluster:",
+                                                                   min = 1, max = 4, value = 2, step = 1)),
+                                      
+                                      conditionalPanel(condition = "input.visualization == 'Rect'",
+                                                       sliderInput("slider_cluster_rect", "Choose the number of cluster",
+                                                                   min = 1, max = 4, value = 2, step = 1),
+                                                       
+                                                       checkboxInput("neurons_grid", strong("Make neurons per grid"), value = TRUE)),
+                                      
+                                      actionButton("showPlot", "Plot")
+                                    
+                                  ),# <- column: Visualization
+                                ),# <- /Fluid Row
+                                
+                                fluidRow(plotOutput("plot"))
+                       )# <- /Tab Panel: Plot
+                       
+              ),#<- /Tab Box
+  
+    ),#<- /Tab Item: Import 
+    # <--------------------- </TAB IMPORT> -------------------------->
+    
+    # <--------------------- <TAB QUALITY CONTROL SAMPLES> -------------------------->
+    tabItem(tabName = "quality",
+            fluidRow(
+              box(width = 4, title = "Input Parameters", solidHeader = TRUE, status = "primary",
+                  numericInput("gridX", "Grid-X:", value = 1),
+                  
+                  numericInput("gridY", "Grid-Y:", value = 1),
+                  
+                  numericInput("learningRate", "Learning Rate", value = 1),
+                  
+                  numericInput("len", "Len:", value = 1),
+                  
+                  selectInput("selectDistance", "Distance:", choices = c("Euclidean","Other"))
+              ),#<- /Box: Input Parameters
+              
+              box(width = 8, title = "SOM Grid", solidHeader = TRUE, status = "primary",
+                  fluidRow(
+                    column(3, offset = 2, selectInput("vegetationIndex", "Vegetation Index", choices = c("NDVI", "Other", "Other2"))),
+                    
+                    column(3, offset = 2, selectInput("typePlot", "Type Plot", choices = c("Patterns", "Other", "Other2")))
+                  )# <- /fluidRow
+              )# <- /Box: Som Grid
+            ),#<- /fluidRow
             
-            tabItem(tabName = "import",
-                        tabBox(width = 13, 
-                               
-                               tabPanel("File Config",
-                                        
-                                        fluidRow(
-                                          
-                                          column(3, fileInput("chooseFile", "Choose File", multiple = FALSE),
-                                                        checkboxInput("header", "Header", TRUE)),
-                                                 
-                                          column(1),
-                                                 
-                                          column(3, radioButtons("separator", "Separator:", 
-                                                                        choices = c(Comma = ",", Semicolon = ";", Tab = "\t"), 
-                                                                        selected = ",")),
-                                                 
-                                          column(3, radioButtons("quote", "Quote:", 
-                                                                        choices = c(None = "", "Double Quote" = '"', "Single Quote" = "'"),
-                                                                        selected = '"')),
-                                                 
-                                          column(2, radioButtons("display", "Display:",
-                                                                        choices = c(Head = "head", All = "all"),
-                                                                        selected = "head"))
-                                          ),#<- /Fluid Row
-                                        
-                                        fluidRow( tableOutput("file"))
-                                        
-                               ),#<- /Tab Panel: File Config
-                               
-                               
-                               
-                               tabPanel("Column View",
-                                        
-                                        fluidRow(
-                                          column(4, checkboxGroupInput("columns", "Select Columns:", choices = NULL))
-                                          ),
-                                        
-                               )#<- /Tab Panel: Column View
-                               
-                      ),#<- /Tab Box
+            fluidRow(box(width = 12, title = "Confusion Between Cluster", status = "primary"))
+             
+    ),# <- /tabItem: Quality
+    
+    tabItem(tabName = "dataTable",
+            fluidRow(
+              box(title = "Define the Sample Quality", solidHeader = TRUE, status = "primary",
+                  
+                  column(4, offset = 1, numericInput("conditional", "Conditional", value = 50)),
+                  
+                  column(4, offset = 1, numericInput("posterior", "Posterior", value = 50))
 
-            )#<- /Tab Item: Import 
+              ),#<- /Box
+              
+              box(title = "Summary", solidHeader = TRUE, status = "primary",
+                  
+                  helpText("X% samples will be kept", br(), 
+                           "Y% samples will be removed", br(),
+                           "Z% samples must be analyzed")
+              ),#<- /Box
+            ),#<- /fluiRow
             
-          )# <- /Tab Items
+            fluidRow(
+              tabBox(width = 12,
+                tabPanel("Input Samples", "Output"),
+                tabPanel("Samples Status", "Output"),
+                tabPanel("Subclasses", "Output"),
+                tabPanel("Output Samples", "Output")
+              )
+            )#<- /fluidRow
+    ),#<- /TabItem: Data Table
+    # <--------------------- </TAB QUALITY CONTROL SAMPLES> -------------------------->
+    
+    # <--------------------- <TAB ST-ANALYSIS> -------------------------->
+    tabItem(tabName = "analyticSimple",
+            fluidRow(
+              box(title = "Choose class be analyzed:", solidHeader = TRUE, status = "primary",
+                  selectInput("class", "", choices = c("Class01", "Class02", "Class03"), selected = NULL)),
+              
+              box(title = "Choose Year", solidHeader = TRUE, status = "primary",
+                  selectInput("year", "",
+                              choices = c("2000","2001", "2002", "2003","2004", "2005","2006", "2007", "2008", "2009", 
+                                          "2010","2011", "2012", "2013", "2014", "2015", "2016", "2017","2018","2019")))
+            ),#<- /fluidRow
+            
+            fluidRow(
+              box(width = 12, title = 'Output', status = "primary",
+                  actionButton("removeFilters", "Remove All Filters"))
+            ),#<- /fluidRow
+            
+            fluidRow(
+              box(width = 12, title = "Plot Samples", status = "primary",
+                  selectInput("index", "Index:", choices = c("NDVI", "other", "other"), selected = "NDVI"))
+            )#<- /fluidRow
+     ),#<- /tabItem: Analytic Simple
+    
+    tabItem(tabName = "exploreSubclasses",
+            fluidRow(
+              box(title = "HClust:", status = "primary", selectInput("class", "Input data: Weigth vectors",
+                                                                      choices = c("Class01", "Class02", "Class03"), selected = NULL)),
+              
+              box(title = "Cluster Dendogram", status = "primary",
+                  
+                  numericInput("numberCluster", "Define Number of cluster:", value = 1),
+                  
+                  numericInput("height","Height:", value = 1))
+            ),#<- /fluidRow
+            
+            fluidRow(
+              box(width = 3, title = "Choose Year", status = "primary",
+                  selectInput("year", "", 
+                              choices = c("2000","2001", "2002", "2003","2004", "2005","2006", "2007", "2008", "2009", 
+                                          "2010","2011", "2012", "2013", "2014", "2015", "2016", "2017","2018","2019"))),
+              
+              box(width = 9, title = 'Output', status = "primary", actionButton("removeFilters", "Remove All Filters"))
+            ),#<- /fluidRow
+            
+            fluidRow(
+              box(width = 12, title = "Plot Samples", status = "primary",
+                  selectInput("index", "Index:", choices = c("NDVI", "other1", "other2"),selected = "NDVI"))
+            )#<- /fluidRow
+     )#<- tabName: Analytic Explore Subclasses
+    
+    # <--------------------- </TAB ST-ANALYSIS> -------------------------->
+    
+    # <----------------------------- </SIDEBAR TABS CONFIGURATION> ------------------------------------> 
+    
+  )# <- /Tab Items
           
-        )
-        #<----  /BODY ---->
+)
+#<---------  /BODY ------------>
 
 dashboardPage(header, sidebar, body)
