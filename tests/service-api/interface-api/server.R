2e8181450760bd479588f2c3391f5bca50170cac
Douglas Henrique <barbozadh99@gmail.com>
2020-11-13 15:39:41 -0300

    update

--- tests/service-api/interface-api/server.R
+++ tests/service-api/interface-api/server.R
@@ -8,13 +8,33 @@ shinyServer(function(input, output){
     
     req(input$file)
     
-    read.csv(
+    file_import <- read.csv(
       input$file$datapath,
       header = TRUE,
       sep = ",",
       quote = '"'
-    ) 
+    )
     
-  })
+  # API request: 
+    
+    # output$plot <- renderPlot({
+    #   
+    #   url_config <- httr::modify_url(url = "http://127.0.0.1:8888/",
+    #                           path = "clustering/som/obj",
+    #                           query = list(xdim = 6,
+    #                                        ydim = 4,
+    #                                        rlen= 1,
+    #                                        alpha = 0.5))
+    #   
+    #   request_som <- httr::POST(url = url_config,
+    #                             body = file_import,
+    #                             encode = "json",
+    #                             httr::verbose())
+    #   
+    #   kohonen_obj <- unserialize(httr::content(request_som, as = "raw"))
+    #   
+    # })# <- output: Plot
+  
+  })# <- /output: Table
   
 })
